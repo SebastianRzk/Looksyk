20d940b75737c08653b520c0898edc5e6d100eab
SebastianRzk <github@sebastianruziczka.de>
2025-02-16 09:36:29 +0100

    fix query unknown message

--- backend/src/looksyk/queries/available.rs
+++ backend/src/looksyk/queries/available.rs
@@ -1,3 +1,4 @@
+use crate::looksyk::queries::blocks::QUERY_NAME_BLOCKS;
 use crate::looksyk::queries::insert_file_content::QUERY_NAME_INSERT_FILE_CONTENT;
 use crate::looksyk::queries::pagehierarchy::QUERY_NAME_PAGE_HIERARCHY;
 use crate::looksyk::queries::references_to::QUERY_NAME_REFERENCES_TO;
@@ -5,7 +6,8 @@ use crate::looksyk::queries::todo::QUERY_NAME_TODOS;
 
 pub fn available_query_types() -> String {
     format!(
-        "{}, {}, {}, {}",
+        "{}, {}, {}, {}, {}",
+        QUERY_NAME_BLOCKS,
         QUERY_NAME_PAGE_HIERARCHY,
         QUERY_NAME_REFERENCES_TO,
         QUERY_NAME_TODOS,
