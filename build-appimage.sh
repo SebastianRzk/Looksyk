0f79e4353f9b213a19a4de56318038cf3b872c15
SebastianRzk <github@sebastianruziczka.de>
2025-09-03 11:44:58 +0200

    add appinfo / metainfo xml

--- build-appimage.sh
+++ build-appimage.sh
@@ -8,6 +8,7 @@ mkdir -p "$APPDIR/usr/bin"
 mkdir -p "$APPDIR/usr/share/looksyk/static"
 mkdir -p "$APPDIR/usr/share/icons/hicolor/256x256/apps"
 mkdir -p "$APPDIR/usr/share/licenses/looksyk"
+mkdir -p "$APPDIR/usr/share/metainfo"
 
 cp target/looksyk "$APPDIR/usr/bin/looksyk-backend"
 cp application-wrapper/Looksyk-AppRun "$APPDIR/AppRun"
@@ -19,6 +20,8 @@ fi
 cp -r target/static/* "$APPDIR/usr/share/looksyk/static/"
 
 cp -r target/application-wrapper/* "$APPDIR/usr/share/looksyk/"
+
+cp de.sebastianruziczka.looksyk.metainfo.xml "$APPDIR/usr/share/metainfo/de.sebastianruziczka.looksyk.appdata.xml"
 
 cp icon/Looksyk_256.png "$APPDIR/usr/share/icons/hicolor/256x256/apps/de.sebastianruziczka.looksyk.png"
 
